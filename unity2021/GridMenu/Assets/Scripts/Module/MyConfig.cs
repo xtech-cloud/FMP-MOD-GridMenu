1084046f4b2130c8ea971a990a46780d51a1ad1d
Niall Liu <easlee@outlook.com>
2023-04-01 10:49:23 +0800

    generated

--- unity2021/GridMenu/Assets/Scripts/Module/MyConfig.cs
+++ unity2021/GridMenu/Assets/Scripts/Module/MyConfig.cs
@@ -8,10 +8,73 @@ namespace XTC.FMP.MOD.GridMenu.LIB.Unity
     /// </summary>
     public class MyConfig : MyConfigBase
     {
+        public class CellContent
+        {
+            [XmlAttribute("type")]
+            public string type { get; set; } = "";
+            [XmlArray("ParameterS"), XmlArrayItem("Parameter")]
+            public Parameter[] parameterS { get; set; } = new Parameter[0];
+            [XmlArray("SubjectS"), XmlArrayItem("Subject")]
+            public Subject[] subjectS { get; set; } = new Subject[0];
+        }
+
+        public class Background
+        {
+            [XmlAttribute("image")]
+            public string image { get; set; } = "";
+        }
+
+
+        public class Debug
+        {
+            [XmlAttribute("active")]
+            public bool active { get; set; } = false;
+            [XmlAttribute("lineColor")]
+            public string lineColor { get; set; } = "#FF0000FF";
+            [XmlAttribute("cellColor")]
+            public string cellColor { get; set; } = "#00FF0088";
+            [XmlAttribute("drawLine")]
+            public bool drawLine { get; set; } = true;
+            [XmlAttribute("drawCell")]
+            public bool drawCell { get; set; } = true;
+        }
+
+        public class Cell
+        {
+            [XmlAttribute("name")]
+            public string name { get; set; } = "";
+            [XmlAttribute("columnStart")]
+            public int columnStart { get; set; } = 0;
+            [XmlAttribute("columnEnd")]
+            public int columnEnd { get; set; } = 0;
+            [XmlAttribute("rowStart")]
+            public int rowStart { get; set; } = 0;
+            [XmlAttribute("rowEnd")]
+            public int rowEnd { get; set; } = 0;
+            [XmlElement("Content")]
+            public CellContent content { get; set; } = new CellContent();
+        }
+
+        public class Grid
+        {
+            [XmlAttribute("column")]
+            public int column { get; set; } = 48;
+            [XmlAttribute("row")]
+            public int row { get; set; } = 27;
+        }
+
         public class Style
         {
             [XmlAttribute("name")]
             public string name { get; set; } = "";
+            [XmlElement("Background")]
+            public Background background { get; set; } = new Background();
+            [XmlElement("Grid")]
+            public Grid grid { get; set; } = new Grid();
+            [XmlArray("CellS"), XmlArrayItem("Cell")]
+            public Cell[] cellS { get; set; } = new Cell[0];
+            [XmlElement("Debug")]
+            public Debug debug { get; set; } = new Debug();
         }
 
 
--- unity2021/GridMenu/Assets/Scripts/Module/MyInstance.cs
+++ unity2021/GridMenu/Assets/Scripts/Module/MyInstance.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 using LibMVCS = XTC.FMP.LIB.MVCS;
 using XTC.FMP.MOD.GridMenu.LIB.Proto;
 using XTC.FMP.MOD.GridMenu.LIB.MVCS;
+using System;
+using UnityEngine.Video;
+using System.Collections;
 
 namespace XTC.FMP.MOD.GridMenu.LIB.Unity
 {
@@ -15,6 +18,32 @@ namespace XTC.FMP.MOD.GridMenu.LIB.Unity
     /// </summary>
     public class MyInstance : MyInstanceBase
     {
+        public class CarouselTask
+        {
+            public string type = "";
+            public string uri = "";
+            public int duration = 0;
+        }
+
+        public class CarouselTaskPool
+        {
+            public int timer = 0;
+            public int activeIndex = 0;
+            public GameObject gameObject;
+            public List<CarouselTask> taskS = new List<CarouselTask>();
+        }
+
+        public class UiReference
+        {
+            public RectTransform rtCellTemplate;
+        }
+
+        private UiReference uiReference_ = new UiReference();
+        private bool isOpened_ = false;
+        /// <summary>
+        /// key：gameobject的instanceId
+        /// </summary>
+        private Dictionary<int, CarouselTaskPool> carouselTaskPoolS_ = new Dictionary<int, CarouselTaskPool>();
 
         public MyInstance(string _uid, string _style, MyConfig _config, MyCatalog _catalog, LibMVCS.Logger _logger, Dictionary<string, LibMVCS.Any> _settings, MyEntryBase _entry, MonoBehaviour _mono, GameObject _rootAttachments)
             : base(_uid, _style, _config, _catalog, _logger, _settings, _entry, _mono, _rootAttachments)
@@ -29,6 +58,19 @@ namespace XTC.FMP.MOD.GridMenu.LIB.Unity
         /// </remarks>
         public void HandleCreated()
         {
+            uiReference_.rtCellTemplate = rootUI.transform.Find("templateCell").GetComponent<RectTransform>();
+            uiReference_.rtCellTemplate.gameObject.SetActive(false);
+            loadTextureFromTheme(style_.background.image, (_texture) =>
+            {
+                var imgBackground = rootUI.transform.Find("imgBackground").GetComponent<RawImage>();
+                imgBackground.texture = _texture;
+            }, () => { });
+
+            createCellS();
+
+            rootUI.transform.Find("debug_imgGrid").gameObject.SetActive(style_.debug.active);
+            if (style_.debug.active)
+                drawDebugGridLine();
         }
 
         /// <summary>
@@ -48,6 +90,16 @@ namespace XTC.FMP.MOD.GridMenu.LIB.Unity
         {
             rootUI.gameObject.SetActive(true);
             rootWorld.gameObject.SetActive(true);
+            isOpened_ = true;
+
+
+            foreach (var pair in carouselTaskPoolS_)
+            {
+                carouselTaskPoolS_[pair.Key].activeIndex = 0;
+                carouselTaskPoolS_[pair.Key].timer = 0;
+                refreshCarousel(pair.Key);
+            }
+            mono_.StartCoroutine(updateCarouselTick());
         }
 
         /// <summary>
@@ -57,6 +109,201 @@ namespace XTC.FMP.MOD.GridMenu.LIB.Unity
         {
             rootUI.gameObject.SetActive(false);
             rootWorld.gameObject.SetActive(false);
+            isOpened_ = false;
+        }
+
+        private void createCellS()
+        {
+
+            var rtRootUI = rootUI.GetComponent<RectTransform>();
+            int rootWidth = (int)rtRootUI.rect.width;
+            int rootHeight = (int)rtRootUI.rect.height;
+            int gridWidth = rootWidth / style_.grid.column;
+            int gridHeight = style_.grid.row == 0 ? gridWidth : rootHeight / style_.grid.row;
+            Action<GameObject, string> buildImage = (_obj, _image) =>
+            {
+                var img = _obj.AddComponent<Image>();
+                img.preserveAspect = true;
+                if (!string.IsNullOrEmpty(_image))
+                {
+                    loadTextureFromTheme(_image, (_texture) =>
+                    {
+                        img.sprite = Sprite.Create(_texture, new Rect(0, 0, _texture.width, _texture.height), new Vector2(0.5f, 0.5f));
+                    }, () => { });
+                }
+            };
+
+            Action<GameObject, string> buildVideo = (_obj, _uri) =>
+            {
+                var vp = _obj.AddComponent<VideoPlayer>();
+                vp.source = VideoSource.Url;
+                if (!string.IsNullOrEmpty(_uri))
+                {
+                    vp.url = _uri;
+                }
+                var size = _obj.GetComponent<RectTransform>().rect.size;
+                RenderTexture renderTexture = new RenderTexture((int)size.x, (int)size.y, 32);
+                vp.targetTexture = renderTexture;
+                vp.playOnAwake = false;
+
+                var image = _obj.AddComponent<RawImage>();
+                image.texture = renderTexture;
+            };
+            foreach (var cell in style_.cellS)
+            {
+                int x = (cell.columnStart - 1) * gridWidth;
+                int width = (cell.columnEnd + 1 - cell.columnStart) * gridWidth;
+                int y = -((cell.rowStart - 1) * gridHeight);
+                int height = (cell.rowEnd + 1 - cell.rowStart) * gridHeight;
+                var cloneCell = GameObject.Instantiate(uiReference_.rtCellTemplate.gameObject, uiReference_.rtCellTemplate.parent);
+                cloneCell.name = cell.name;
+                var rtCloneCell = cloneCell.GetComponent<RectTransform>();
+                rtCloneCell.anchoredPosition = new Vector2(x, y);
+                rtCloneCell.sizeDelta = new Vector2(width, height);
+                if (cell.content.type == "Image")
+                {
+                    var imageValue = tryParseFromParameter<string>(cell.content.parameterS, "image");
+                    buildImage(cloneCell, imageValue);
+                }
+                else if (cell.content.type == "Button")
+                {
+                    var imageValue = tryParseFromParameter<string>(cell.content.parameterS, "image");
+                    buildImage(cloneCell, imageValue);
+                    var btn = cloneCell.AddComponent<Button>();
+                    btn.targetGraphic = cloneCell.GetComponent<RawImage>();
+                }
+                else if (cell.content.type == "Carousel")
+                {
+                    buildVideo(cloneCell, "");
+                    int instanceID = cloneCell.GetInstanceID();
+                    if (!carouselTaskPoolS_.ContainsKey(instanceID))
+                    {
+                        carouselTaskPoolS_[instanceID] = new CarouselTaskPool();
+                        carouselTaskPoolS_[instanceID].gameObject = cloneCell;
+                    }
+                    var count = tryParseFromParameter<int>(cell.content.parameterS, "count");
+                    for (int i = 0; i < count; i++)
+                    {
+                        var carouselTask = new CarouselTask();
+                        carouselTask.type = tryParseFromParameter<string>(cell.content.parameterS, string.Format("item_{0}_type", i + 1));
+                        carouselTask.uri = tryParseFromParameter<string>(cell.content.parameterS, string.Format("item_{0}_uri", i + 1));
+                        carouselTask.duration = tryParseFromParameter<int>(cell.content.parameterS, string.Format("item_{0}_duration", i + 1));
+                        carouselTaskPoolS_[instanceID].taskS.Add(carouselTask);
+                    }
+                }
+                cloneCell.SetActive(true);
+            }
+        }
+
+        private void drawDebugGridLine()
+        {
+            Color lineColor;
+            if (!ColorUtility.TryParseHtmlString(style_.debug.lineColor, out lineColor))
+            {
+                lineColor = Color.white;
+            }
+            Color cellColor;
+            if (!ColorUtility.TryParseHtmlString(style_.debug.cellColor, out cellColor))
+            {
+                cellColor = Color.white;
+            }
+            var rtGrid = rootUI.transform.Find("debug_imgGrid").GetComponent<RectTransform>();
+            rtGrid.transform.SetAsLastSibling();
+            int width = (int)rtGrid.rect.width;
+            int height = (int)rtGrid.rect.height;
+            int gridWidth = width / style_.grid.column;
+            int gridHeight = style_.grid.row == 0 ? gridWidth : height / style_.grid.row;
+            Texture2D texture = new Texture2D(width, height, TextureFormat.RGBA32, false);
+            for (int x = 0; x < width; ++x)
+            {
+                for (int y = 0; y < height; ++y)
+                {
+                    Color color = new Color(0, 0, 0, 0);
+                    if (x % gridWidth == 0 || y % gridHeight == 0 && style_.debug.drawLine)
+                        color = lineColor;
+                    bool inCell = false;
+                    foreach (var cell in style_.cellS)
+                    {
+                        if (inCell)
+                            continue;
+                        inCell = x >= (cell.columnStart - 1) * gridWidth &&
+                            x < (cell.columnEnd) * gridWidth &&
+                            y >= (cell.rowStart - 1) * gridHeight &&
+                            y < (cell.rowEnd) * gridHeight;
+                    }
+                    if (inCell && style_.debug.drawCell)
+                        color = cellColor;
+                    texture.SetPixel(x, height - y, color);
+                }
+            }
+            texture.Apply();
+            rtGrid.GetComponent<RawImage>().texture = texture;
+        }
+
+        private Type tryParseFromParameter<Type>(MyConfigBase.Parameter[] _parameterS, string _key)
+        {
+            object value = null;
+            foreach (var parameter in _parameterS)
+            {
+                if (parameter.key == _key)
+                {
+                    if (parameter.type == "string")
+                        value = parameter.value;
+                    else if (parameter.type == "int")
+                        value = int.Parse(parameter.value);
+                    else if (parameter.type == "float")
+                        value = float.Parse(parameter.value);
+                    else if (parameter.type == "bool")
+                        value = bool.Parse(parameter.value);
+                    break;
+                }
+            }
+            return (Type)value;
+        }
+
+        private IEnumerator updateCarouselTick()
+        {
+            while (isOpened_)
+            {
+                foreach (var pair in carouselTaskPoolS_)
+                {
+                    var taskPool = carouselTaskPoolS_[pair.Key];
+                    var task = taskPool.taskS[taskPool.activeIndex];
+                    if (taskPool.timer >= task.duration)
+                    {
+                        taskPool.timer = 0;
+                        taskPool.activeIndex += 1;
+                        if (taskPool.activeIndex >= taskPool.taskS.Count)
+                            taskPool.activeIndex = 0;
+                        refreshCarousel(pair.Key);
+                    }
+                    taskPool.timer += 1;
+                }
+                yield return new WaitForSeconds(1);
+            }
+        }
+
+        private void refreshCarousel(int _gameObejctID)
+        {
+            var taskPool = carouselTaskPoolS_[_gameObejctID];
+            var task = taskPool.taskS[taskPool.activeIndex];
+            if (task.type == "Image")
+            {
+                loadTextureFromTheme(task.uri, (_texture) =>
+                {
+                    taskPool.gameObject.GetComponent<RawImage>().texture = _texture;
+                }, () => { });
+            }
+            else if (task.type == "Video")
+            {
+                string path = settings_["path.themes"].AsString();
+                path = System.IO.Path.Combine(path, MyEntryBase.ModuleName);
+                string filefullpath = System.IO.Path.Combine(path, task.uri);
+                var vp = taskPool.gameObject.GetComponent<VideoPlayer>();
+                vp.url = filefullpath;
+                taskPool.gameObject.GetComponent<RawImage>().texture = vp.targetTexture;
+                vp.Play();
+            }
         }
     }
 }
